daea62559894d9a4c302b045ce5e44e3061d9474
tuyendung1 <130293048+tuyendung1@users.noreply.github.com>
2025-12-14 19:40:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 var cors = require('cors');
 let svgCaptcha = require('svg-captcha');
 let svg2img    = require('svg2img');
-let Create     = function(client, name){
-	let captcha = svgCaptcha.create({background:'#99CC99', noise:0});
-	svg2img(captcha.data, function(error, buffer) {
-		client.captcha = captcha.text;
-		console.log("client.captcha", client.captcha);
+async function Create( name) {
+	const captcha = svgCaptcha.create({background:'#99CC99', noise:0});
+	const buffer = await new Promise((resolve, reject) => {
+    svg2img(captcha.data, (err, buf) => {
+      if (err) return reject(err);
+      resolve(buf);
+    });
+  });
+	
+		captcha = captcha.text;
+		console.log("client.captcha", captcha);
 		let data = {};
 		data['data'] = 'data:image/png;base64,' + buffer.toString('base64');
 		data['name'] = name;
 		console.log("client.captcha",data);
-		client.red({captcha: data});
-	});
+		//client.red({captcha: data});
 }
+Create("hQ");
 let express       = require('express');
 let app           = express();
 //let server 	  	  = https.createServer(credentials, app);
